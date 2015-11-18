b3b06c2d5ec3b66bbc47685db6dc235291a5b09e
jeffrey475 <jeffrey475@yahoo.com>
2015-11-18 11:58:54 -0800

    Update Main.java

--- src/edu/cpp/cs/cs141/prog_assgmnt_final/Main.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_final/Main.java
@@ -1,8 +1,9 @@
 /**
- * 
+ * This is tbhe main class
  */
 package edu.cpp.cs.cs141.prog_assgmnt_final;
 
+//Imports the random class
 import java.util.Random;
 
 /**
@@ -12,9 +13,14 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String args[]) {
-		//Player player = new Player();
-		Map map = new Map();
+		//initializes the game engine
 		GameEngine ge = new GameEngine();
+		//initializes the map
+		Map map = new Map();
+		
+		//ignore for now 
+		
+		//Player player = new Player();
 		//map.generateEmptySpace();
 		//map.generateRoomsWithBriefCase();
 		//map.generatePlayer();
@@ -22,9 +28,9 @@ public class Main {
 		//map.generateRadar();
 		//map.generateAdditionalBullet();
 		//map.generateInvincibility();
-		ge.generateMap();
-		ge.move(3,"P");
-		ge.printMap();
+		//ge.generateMap();
+		//ge.move(3,"P");
+		//ge.printMap();
 		
 		//System.out.println(ge.printMap());
 		
